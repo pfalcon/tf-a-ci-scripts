510e840bc35cee70d5715c5dd18c56f7c92a6d9d
Arthur She <arthur.she@linaro.org>
2023-01-23 13:21:28 -0800

    SC7180: Enable LAVA test job submission
        It runs BL31 boot test on Lazor Chromebook
    
    Signed-off-by: Arthur She <arthur.she@linaro.org>
    Change-Id: Ifad99eb7e357e0608ccab314b7aef87967450fa3

---
+++ script/gen_sc7180_bl31_depthcharge_boot_yaml.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2019-2023 Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# Generate a YAML file in order to dispatch Juno TFTF runs on LAVA. Note that
+# this script would produce a meaningful output when run via. Jenkins.
+#
+# $bin_mode must be set. This script outputs to STDOUT
+
+ci_root="$(readlink -f "$(dirname "$0")/..")"
+source "$ci_root/utils.sh"
+
+get_bl31_url() {
+	local bin_mode="${bin_mode:?}"
+
+	if upon "$jenkins_run"; then
+		echo "$jenkins_url/job/$JOB_NAME/$BUILD_NUMBER/artifact/artefacts/$bin_mode/bl31.elf"
+	else
+		echo "file://$workspace/artefacts/$bin_mode/bl31.elf"
+	fi
+}
+
+bl31_url="${bl31_url:-$(get_bl31_url)}"
+
+build_mode=$(echo $bin_mode | tr '[:lower:]' '[:upper:]')
+
+cat <<EOF
+device_type: sc7180-trogdor-lazor-limozeen
+job_name: SC7180 Chromebook BL31 depthcharge boot test - $build_mode
+timeouts:
+  job:
+    minutes: 30
+  action:
+    minutes: 5
+  connection:
+    minutes: 5
+priority: medium
+visibility: public
+actions:
+- deploy:
+    timeout:
+      minutes: 5
+    to: flasher
+    images:
+      image:
+        url: https://images.validation.linaro.org/people.linaro.org/~arthur.she/images/chromebook/lazor/lazor_tf-a-ci_golden_image.bin.gz
+      bl31:
+        url: $bl31_url
+- boot:
+    timeout:
+      minutes: 2
+    method: minimal
+- test:
+    timeout:
+      minutes: 5
+    monitors:
+    - name: "BL31_boot_test"
+      start: "Initializing runtime services"
+      end: "This is a TF-A test build\\\\. Halting\\\\.{3}"
+      pattern: "Starting depthcharge on Lazor\\\\.{3}"
+      fixupdict:
+          '!': pass
+          '': fail
+
+EOF
