609be18fd79bf47463c96160aa7e0a41b67d8adb
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2023-09-04 17:08:42 +0300

    eclair/build-tfa-multiple.sh: unset ECLAIR_PROJECT_NAME
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: I3940e7d0690f1053415c3d6a75003db732aa2330

--- eclair/build-tfa-multiple.sh
+++ eclair/build-tfa-multiple.sh
@@ -9,6 +9,7 @@ set -ex
 for TF_CONFIG in ${TF_CONFIG_LIST}; do
     echo "============== ${TF_CONFIG} =============="
     . tf-a-ci-scripts/eclair/analyze_common.sh
+    unset ECLAIR_PROJECT_NAME
     detachLicense 3000
     tf-a-ci-scripts/eclair/build-tfa.sh ${TF_CONFIG}
 done
