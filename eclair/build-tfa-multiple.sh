07b8f4978c0353ebcda30bad1d9bb121c5fb5382
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2023-09-04 16:25:29 +0300

    eclair/build-tfa-multiple.sh: Script to build multiple TF-A configs
    This script will be run under eclair_analyze to produce cumulative project
    database for multiple configs, and then cumulative report.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: I751e6fda6e4a8d5e6176944bf2aa57c21c05d824

---
+++ eclair/build-tfa-multiple.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (c) 2023 Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+set -ex
+
+for TF_CONFIG in ${TF_CONFIG_LIST}; do
+    echo "============== ${TF_CONFIG} =============="
+    . tf-a-ci-scripts/eclair/analyze_common.sh
+    detachLicense 3000
+    tf-a-ci-scripts/eclair/build-tfa.sh ${TF_CONFIG}
+done
