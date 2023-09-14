cc219f421eab2c4a2279118f1b8be54e5c73ac07
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2023-09-14 23:10:54 +0300

    eclair/build-tfa-multiple.sh: Count configs being built
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: Iab1b558b81f0d9941ca528a7172d4e73807235d7

--- eclair/build-tfa-multiple.sh
+++ eclair/build-tfa-multiple.sh
@@ -6,9 +6,14 @@
 
 set -ex
 
+num_configs=$(echo ${TF_CONFIG_LIST} | wc -w)
+
+cnt=1
+
 for TF_CONFIG in ${TF_CONFIG_LIST}; do
-    echo "============== ${TF_CONFIG} =============="
+    echo "============== ${TF_CONFIG} (${cnt}/${num_configs}) =============="
     export TF_CONFIG
     detachLicense 3000
     time tf-a-ci-scripts/eclair/build-tfa.sh ${TF_CONFIG}
+    cnt=$((cnt + 1))
 done
