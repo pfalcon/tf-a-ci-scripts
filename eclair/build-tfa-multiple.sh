8d00686293e8f2aba87318b27ae94d42d2d30572
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2023-09-14 11:25:19 +0300

    eclair/build-tfa-multiple.sh: export TF_CONFIG
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    Change-Id: I8f14c462cce6fba7c5ae2be51a10060f8c9694cd

--- eclair/build-tfa-multiple.sh
+++ eclair/build-tfa-multiple.sh
@@ -8,6 +8,7 @@ set -ex
 
 for TF_CONFIG in ${TF_CONFIG_LIST}; do
     echo "============== ${TF_CONFIG} =============="
+    export TF_CONFIG
     detachLicense 3000
     time tf-a-ci-scripts/eclair/build-tfa.sh ${TF_CONFIG}
 done
